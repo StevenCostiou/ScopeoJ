f87fb8c07e214ba4fc6046464441e1dfe0c57910
Steven Costiou <26929529+StevenCostiou@users.noreply.github.com>
2026-02-13 15:21:39 +0100

    cleaning dead code

--- src/main/java/scopeo/model/ScpTrace.java
+++ src/main/java/scopeo/model/ScpTrace.java
@@ -10,42 +10,18 @@ public class ScpTrace {
     private ScpMethod method;
     private ArrayList<ScpObject> arguments = new ArrayList<>();
 
-    public int getTraceIndex() {
-        return traceIndex;
-    }
-
     public void setTraceIndex(int traceIndex) {
         this.traceIndex = traceIndex;
     }
-
-    public ScpObject getReceiver() {
-        return receiver;
-    }
-
     public void setReceiver(ScpObject receiver) {
         this.receiver = receiver;
     }
-
-    public ScpObject getSender() {
-        return sender;
-    }
-
     public void setSender(ScpObject sender) {
         this.sender = sender;
     }
-
-    public ScpObject getResult() {
-        return result;
-    }
-
     public void setResult(ScpObject result) {
         this.result = result;
     }
-
-    public ScpMethod getMethod() {
-        return method;
-    }
-
     public void setMethod(ScpMethod method) {
         this.method = method;
     }
