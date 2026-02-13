dec570844933b8e99735d1a3367d5d2e9f10e6c8
Steven Costiou <26929529+StevenCostiou@users.noreply.github.com>
2026-02-13 15:20:33 +0100

    cleaning dead code

--- src/main/java/scopeo/model/ScpMethod.java
+++ src/main/java/scopeo/model/ScpMethod.java
@@ -8,44 +8,10 @@ public class ScpMethod {
     private String fileName;
     private int line;
 
-    public String getDefiningClassName() {
-        return definingClassName;
-    }
-
-    public void setDefiningClassName(String definingClassName) {
-        this.definingClassName = definingClassName;
-    }
-
-    public String getSignature() {
-        return signature;
-    }
-
-    public void setSignature(String signature) {
-        this.signature = signature;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public int getLine() {
-        return line;
-    }
-
-    public void setLine(int line) {
-        this.line = line;
-    }
-
     public ScpMethod(JoinPoint callPoint) {
-
         signature = callPoint.getSignature().toString();
         definingClassName = callPoint.getSourceLocation().getWithinType().getCanonicalName();
         fileName = callPoint.getSourceLocation().getFileName();
         line = callPoint.getSourceLocation().getLine();
     }
-
 }
