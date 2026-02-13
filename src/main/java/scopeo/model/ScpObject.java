f0767d9f225a98dcae9652801a3c9004b06d1e9f
Steven Costiou <26929529+StevenCostiou@users.noreply.github.com>
2026-02-13 15:21:21 +0100

    cleaning dead code

--- src/main/java/scopeo/model/ScpObject.java
+++ src/main/java/scopeo/model/ScpObject.java
@@ -4,28 +4,12 @@ public class ScpObject {
     private String className;
     private int objectID;
 
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
-    public int getObjectID() {
-        return objectID;
-    }
-
-    public void setObjectID(int objectID) {
-        this.objectID = objectID;
-    }
-
     public ScpObject(Object target) {
         try {
-            this.setClassName(target.getClass().getCanonicalName());
-            this.setObjectID(target.hashCode());
+            className = target.getClass().getCanonicalName();
+            objectID = target.hashCode();
         } catch (NullPointerException e) {
-            this.setClassName("null");
+            className = "null";
         } catch (java.lang.Throwable e) {
             throw new RuntimeException(e);
         }
